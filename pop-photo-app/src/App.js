9cfa5db6b7daba4206444d9a80c8b2d1f5efac70
Chrisclrk9 <chrisclrk9@gmail.com>
2018-11-26 15:28:35 +0000

    here we are again

--- pop-photo-app/src/App.js
+++ pop-photo-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -6,7 +7,18 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-        <h1>Hello World!</h1>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
         </header>
       </div>
     );
